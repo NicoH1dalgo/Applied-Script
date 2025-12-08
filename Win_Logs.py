62693086190b99ed37f8ba707f7642ac3b27392f
Shuti-Playbox <nh@hidalgo.se>
2025-12-08 21:30:01 +0100

    Fixed comments and overall structure and changed some variable names to more obvious ones

--- Win_Logs.py
+++ Win_Logs.py
@@ -1,27 +1,37 @@
+#   win_logs.py
+#   Script för att läsa windows säkerhetsloggar med hjälp av pywin32
+#   Måste som köras administratör
+#   
+#   Skriven av Nicolas.H
+
+# --Imports-- #
 import win32evtlog
+
+# --Variables-- #
 i = 0
-# öpnna securityloggen
-handle = win32evtlog.OpenEventLog(None, "Security")
-# sätta flaggar för att läsa loggar
-flags = win32evtlog.EVENTLOG_FORWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
-# skapa en lista för att lagra loggar
-all_records = []
-# läsa loggarna
+handle = win32evtlog.OpenEventLog(None, "Security")                              # öpnnar securityloggen på den lockala datorn
+flags = win32evtlog.EVENTLOG_FORWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ  # sätta flagga för hur loggarna ska läsas
+all_records = []                                                                 # skapa en lista för att lagra loggar
+
+
+
+
+# --main code-- #
 while True:
-    records = win32evtlog.ReadEventLog(handle, flags, 0)
-    if not records:
+    sec_logs = win32evtlog.ReadEventLog(handle, flags, 0)    # läser loggarna från handle, hur den läser dem, och från vilken position
+    if not sec_logs:
         break
-    all_records.extend(records)
-# Process and print each record
-for record in all_records:
-    #  kommer ta bort den senare bara för att se att det funkar för nu
-    print(f"Event ID: {record.EventID & 0xFFFF}")
+    all_records.extend(sec_logs)                             # lägger till de lästa loggarna i listan all_records
+
+for sec_logs in all_records:
+    print(f"Event ID: {sec_logs.EventID & 0xFFFF}")          # skriver ut event ID  // kommer ta bort den senare bara för att se att det funkar för nu
+  
+
     
     
     
     i += 1
     
-print(f"{i} Log records found")
+print(f"{i} Loggar.")                               # skriver ut hur många loggar som hittades
 
-# Close the event log handle
-win32evtlog.CloseEventLog(handle)
+win32evtlog.CloseEventLog(handle) #Stänger ner loggen
