9656fa6d81a2c00dddaf2d9c9f13f2bfc83010f1
Shuti <nh@hidalgo.se>
2025-12-15 11:32:45 +0100

    info script

---
+++ ö1:Info_Script/script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Detta script samlar in systeminformation - RECON
+#
+# Kan användas för följande attacker:
+# [Skriv möjliga attacker]
+#
+# Author: Frans Schartau
+# Last Update: 2025-01-01
+
+
+echo "Välkommen till RECON script för att kontrollera en Linux-miljö"
+
+echo
+echo "=== SYSTEMINFO ==="
+uname -a
+
+echo
+echo "=== AKTUELL ANVÄNDARE ==="
+echo $USER
+
+echo
+echo "=== ANVÄNDARE MED SHELL ==="
+grep "sh$" /etc/passwd
+
+echo
+echo "=== NÄTVERK ==="
+ip a | grep inet
+
+echo
+echo "=== Cheat sheet  ==="
+curl cheat.sh
+
+
