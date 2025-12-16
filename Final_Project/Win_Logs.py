9e57e0bf73f850d9c3a5a724d228475e6588b9b5
Nicolas.H <nh@hdialgo.se>
2025-12-16 10:38:24 +0100

    Added some error handling, Added the help page, Added Logging for actions taken

--- Final_Project/Win_Logs.py
+++ Final_Project/Win_Logs.py
@@ -1,35 +1,46 @@
-#   win_logs.py
-#
-#   Script för att läsa och filtrera windows säkerhetsloggar med hjälp av pywin32
-#   Skickar även ett mail om "onormalt" beteende uppstår baserat på regler
-#   Måste som köras administratör och kräver pywin32
-#   
-#   ----------## Skriven av Nicolas.H ##----------
+# --------------------------------- Win_logs.py -------------------------------------- #
+# ------------------------------------------------------------------------------------ #
+# --  Script för att läsa och filtrera windows säkerhetsloggar med hjälp av pywin32 -- #
+# -----  Skickar även ett mail om "onormalt" beteende uppstår baserat på regler ------ #
+# ------------------ Måste som köras administratör och kräver pywin32 ---------------- #
+# ------------------------------------------------------------------------------------ # 
+#  ------------------------## Skriven av Nicolas.H ##--------------------------------- #
 
-# --Imports-- #
+            ## --Imports-- ##
 from itertools import groupby
 import win32evtlog
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import sys
-import ctypes
+from datetime import datetime
 
+            ## --Variables--##
+today_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+
+            ## --Script Start Message-- ##
 print("=" * 25 + "Windows Security Log Scanner" + "=" * 25)
+with open("Logs.txt","a") as file:
+    file.write(f"\n[{today_date}] Script was Initialized")
 
 
+
+            ## --Menu Creation-- ##
 while True:
     menu_option = input("What would you like to do today? \n1. Perform Security log scan\n2. Help page\n3. Version\n4. Quit\n")
+    with open("Logs.txt", "a") as file:
+        file.write(f"\n[{today_date}] Menu Option written: {menu_option}")
     
-   
-    if menu_option  == "1":
-        # --Log Reading Settings-- #
-        if ctypes.windll.shell32.IsUserAnAdmin() == 1 :
-            handle = win32evtlog.OpenEventLog(None, "Security")                                  # öpnnar securityloggen på den lokala datorn
-            flags = win32evtlog.EVENTLOG_FORWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ    # sätta flagga för hur loggarna ska läsas
-            sec_records = []                                                                     # skapa en lista för att lagra loggar
 
-            # --Email config-- #
+            ## --Log Reading Settings-- ##
+    if menu_option  == "1":
+        try: 
+            handle = win32evtlog.OpenEventLog(None, "Security")                                         # öpnnar securityloggen på den lokala datorn
+            flags = win32evtlog.EVENTLOG_FORWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ           # sätta flagga för hur loggarna ska läsas
+            sec_records = []                                                                            # skapa en lista för att lagra loggar
+
+            ## --Email config-- ##
             sender_email = ""
             receiver_mail = ""
             password = ""
@@ -49,55 +60,82 @@ while True:
            
            
            
-            # --Main code-- #
-            if sys.platform == "win32":
+            ## --Main code-- ##
+            try:
                 while True:
-                    sec_logs = win32evtlog.ReadEventLog(handle, flags, 0)                            # läser loggarna från handle, hur den läser dem, och från vilken position
-                    if not sec_logs:                                                                 # Om det inte finns några logs kvar att läsa hoppa ur loopen
-                        break                                                                        # 
-                    sec_records.extend(sec_logs)                                                     # lägger till de lästa loggarna i listan all_records
+                    sec_logs = win32evtlog.ReadEventLog(handle, flags, 0)                               # läser loggarna från handle, hur den läser dem, och från vilken position
+                    if not sec_logs:                                                                    # Om det inte finns några logs kvar att läsa hoppa ur loopen
+                        break                                                                           # 
+                    sec_records.extend(sec_logs)                                                        # lägger till de lästa loggarna i listan all_records
 
-                evt_id = [sec_logs.EventID & 0xFFFF for sec_logs in sec_records]                     # Skapar en lista med endast event ID från alla loggar
-                for event,group in groupby(evt_id):                                                  # Grupperar loggarna baserat på event ID
-                    if event == 4625:                                                                # Kollar om event ID är 4625 (Failed logon)
-                        fail_log = sum(1 for _ in group)                                             # Räknar antalet misslyckade inloggningsförsök
-                        if fail_log >= 5:                                                             # om misslyckade inloggningsförsök är större än 5 så skicka mail AKA alert
-                            # --Create Email Content-- #
-                            subject = f"Event ID : {event} Multiple Failed logon attempts! *HIGH*"
+                evt_id = [sec_logs.EventID & 0xFFFF for sec_logs in sec_records]                        # Skapar en lista med endast event ID från alla loggar och grupperar dem baserat på ID
+                for event,group in groupby(evt_id):                                                     # 
+                    if event == 4625:                                                                   # Kollar om event ID är 4625 och räknar antalet misslyckade inloggningsförsök
+                        fail_log = sum(1 for _ in group)                                                # 
+                        if fail_log >= 5:                                                               # om misslyckade inloggningsförsök är större än 5 så skicka mail AKA alert
+                            
+                            ## --Create Email Content-- ##
+                            subject = f" !! Event ID : {event} Multiple Failed logon attempts! *HIGH* !! "
                             body =  "Irregular activity has been detected investigate promptly!"
                             mail_func()
                             print(subject)
-                            break
+                            with open("Logs.txt", "a") as file:
+                                file.write(f"\n[{today_date}] Event ID {event}: alert was triggered")
 
                     elif event == 1102:
                         fail_log = sum(1 for _ in group)
                         if fail_log >= 1:
-                            # --Create Email Content-- #
+                            
+                            ## --Create Email Content-- ##
                             subject = f"Event ID : {event} Audit log has been cleared *HIGH*"
                             body =  "Irregular activity has been detected investigate promptly!"
                             mail_func()
                             print(subject)
-                            break
-            else:
-                print(f"\033[44mYou cannot run this script on {sys.platform}, switch to Windows and try again\033[0m")
-                break
+                            with open() as file:
+                                file.write(f"\n[{today_date}] Event ID {event}: alert was triggered")
+            except:
+                print(f"\n[{today_date}] Error 666: Operative system is {sys.platform}")
+                with open("Logs.txt","a") as file:
+                    file.write(f"\n[{today_date}] Error 999: Script was run without Admin priviledges")
             
-            win32evtlog.CloseEventLog(handle)                                                         # Stänger ner loggen
-        
-        else:
-            print("\033[44mYou are not an Administrator, Script can only be run with administrator priviledges!\033[0m")
+            
+                win32evtlog.CloseEventLog(handle)                                                         # Stänger ner loggen
+        except:
+            print(f"Error 999: Script was not run with Administrator priviledges!")
+            with open("Logs.txt","a") as file:
+                file.write(f"\n[{today_date}] Error 999: Script was run without Admin priviledges")
             break
+    
+    
+    
+    
+    
+            # --Menu Options-- #
     elif menu_option == "2":
-        print("")
-        break
+        print("=" * 25 + " Help page " + 25 * "=")
+        with open("Help_Page.txt","r", encoding ="UTF-8") as file:
+            for line in file:
+                print(line.strip())
+        print("=" * 25 + " Help page " + 25 * "=")
+
+        with open("Logs.txt","a") as file:
+            file.write(f"\n[{today_date}] User accessed help page")
+    
     elif menu_option == "3":
-        print("Version 1.3.3.7")
-        break
+        print("=" * 25 + " Version 1.3.3.7 " + 25 * "=")
+        with open("Logs.txt","a") as file:
+            file.write(f"\n[{today_date}] User accesed script version")
+
     elif menu_option == "4":
-        print("Goodbye!")
+        print("=" * 25 + " Goodbye! " + "=" * 25)
+        with open("Logs.txt","a") as file:
+            file.write(f"\n[{today_date}] User terminated script")
         break
+    
     else:
-        print("You must choose one of the optiones 1, 2 or 3 !")
+        print("=" * 25 + "You must choose one of the optiones 1, 2 or 3 !" + 25 * "=")
+        with open("Logs.txt","a") as file:
+            file.write(f"\n[{today_date}] Incorrect menu choice")
         continue
     
         
