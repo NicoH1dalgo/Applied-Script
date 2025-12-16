df4ec04f59f3d7066a51a267575133500046bb1f
Nicolas.H <nh@hdialgo.se>
2025-12-16 13:51:07 +0100

    fixed some weird stuff no clue ngl at this point

--- Final_Project/Win_Logs.py
+++ Final_Project/Win_Logs.py
@@ -14,6 +14,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import sys
 from datetime import datetime
+import ctypes
+
 
             ## --Variables--##
 today_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
@@ -28,46 +30,49 @@ with open("Logs.txt","a") as file:
 
             ## --Menu Creation-- ##
 while True:
-    menu_option = input("What would you like to do today? \n1. Perform Security log scan\n2. Help page\n3. Version\n4. Quit\n")
-    with open("Logs.txt", "a") as file:
-        file.write(f"\n[{today_date}] Menu Option written: {menu_option}")
-    
+    if sys.platform == "win32":
+        menu_option = input("What would you like to do today? \n1. Perform Security log scan\n2. Help page\n3. Version\n4. Quit\n")
+        with open("Logs.txt", "a") as file:
+            file.write(f"\n[{today_date}] Menu Option written: {menu_option}")
+        
 
             ## --Log Reading Settings-- ##
-    if menu_option  == "1":
-        try: 
-            handle = win32evtlog.OpenEventLog(None, "Security")                                         # öpnnar securityloggen på den lokala datorn
-            flags = win32evtlog.EVENTLOG_FORWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ           # sätta flagga för hur loggarna ska läsas
-            sec_records = []                                                                            # skapa en lista för att lagra loggar
-
-            ## --Email config-- ##
-            sender_email = ""
-            receiver_mail = ""
-            password = ""
-
-            def mail_func():                                                                      
-                message = MIMEMultipart()
-                message["From"] = sender_email
-                message["To"] = receiver_mail
-                message["Subject"] = subject
-                message.attach(MIMEText(body, "plain"))
-                with smtplib.SMTP("smtp.gmail.com", 587) as server:
-                    server.starttls()
-                    server.login(sender_email, password)  
-                    server.send_message(message)
+        if menu_option  == "1":
+            try: 
+                handle = win32evtlog.OpenEventLog(None, "Security")                                         # öpnnar securityloggen på den lokala datorn
+                flags = win32evtlog.EVENTLOG_FORWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ           # sätta flagga för hur loggarna ska läsas
+                sec_records = []
                 
+            
+            
+                        ## --Email config-- ##
+                sender_email = "mailscriptcs25@gmail.com"
+                receiver_mail = "mailscriptcs25@gmail.com"
+                password = "iuhc wzmw lriv wgbx"
 
-           
-           
-           
-            ## --Main code-- ##
-            try:
+                            
+                def mail_func():                                                                      
+                    message = MIMEMultipart()
+                    message["From"] = sender_email
+                    message["To"] = receiver_mail
+                    message["Subject"] = subject
+                    message.attach(MIMEText(body, "plain"))
+                    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+                        server.starttls()
+                        server.login(sender_email, password)  
+                        server.send_message(message)
+            
+                
+            
+                ## --Main code-- ##
+
                 while True:
                     sec_logs = win32evtlog.ReadEventLog(handle, flags, 0)                               # läser loggarna från handle, hur den läser dem, och från vilken position
                     if not sec_logs:                                                                    # Om det inte finns några logs kvar att läsa hoppa ur loopen
                         break                                                                           # 
                     sec_records.extend(sec_logs)                                                        # lägger till de lästa loggarna i listan all_records
 
+                
                 evt_id = [sec_logs.EventID & 0xFFFF for sec_logs in sec_records]                        # Skapar en lista med endast event ID från alla loggar och grupperar dem baserat på ID
                 for event,group in groupby(evt_id):                                                     # 
                     if event == 4625:                                                                   # Kollar om event ID är 4625 och räknar antalet misslyckade inloggningsförsök
@@ -93,58 +98,58 @@ while True:
                             print(subject)
                             with open() as file:
                                 file.write(f"\n[{today_date}] Event ID {event}: alert was triggered")
-            except:
-                print(f"\n[{today_date}] Error 666: Operative system is {sys.platform}")
+
+                win32evtlog.CloseEventLog(handle)                 
+            except Exception as error:
+                print(f"Error 999: Script was not run with Administrator priviledges!")
+                print(error)
                 with open("Logs.txt","a") as file:
-                    file.write(f"\n[{today_date}] Error 999: Script was run without Admin priviledges")
-            
-            
-                win32evtlog.CloseEventLog(handle)                                                         # Stänger ner loggen
-        except:
-            print(f"Error 999: Script was not run with Administrator priviledges!")
+                    file.write(f"\n[{today_date}] Error 999: Script was run without Admin priviledges")                                                                            # skapa en lista för att lagra loggar
+                break
+
+                # --Menu Options-- #
+        elif menu_option == "2":
+            try:
+                print("=" * 25 + " Help page " + 25 * "=")
+                with open("Help_Page.txt","r", encoding ="UTF-8") as file:
+                    for line in file:
+                        print(line.strip())
+                print("=" * 25 + " Help page " + 25 * "=")
+                with open("Logs.txt","a") as file:
+                    file.write(f"\n[{today_date}] User accessed help page")
+            except FileNotFoundError:
+                print("Error 404 :File not found")
+                break
+        
+        elif menu_option == "3":
+            print("=" * 25 + " Version 1.3.3.7 " + 25 * "=")
             with open("Logs.txt","a") as file:
-                file.write(f"\n[{today_date}] Error 999: Script was run without Admin priviledges")
+                file.write(f"\n[{today_date}] User accesed script version")
+
+        elif menu_option == "4":
+            print("=" * 25 + " Goodbye! " + "=" * 25)
+            with open("Logs.txt","a") as file:
+                file.write(f"\n[{today_date}] User terminated script")
             break
-    
-    
-    
-    
-    
-            # --Menu Options-- #
-    elif menu_option == "2":
-        print("=" * 25 + " Help page " + 25 * "=")
-        with open("Help_Page.txt","r", encoding ="UTF-8") as file:
-            for line in file:
-                print(line.strip())
-        print("=" * 25 + " Help page " + 25 * "=")
 
-        with open("Logs.txt","a") as file:
-            file.write(f"\n[{today_date}] User accessed help page")
-    
-    elif menu_option == "3":
-        print("=" * 25 + " Version 1.3.3.7 " + 25 * "=")
-        with open("Logs.txt","a") as file:
-            file.write(f"\n[{today_date}] User accesed script version")
-
-    elif menu_option == "4":
-        print("=" * 25 + " Goodbye! " + "=" * 25)
-        with open("Logs.txt","a") as file:
-            file.write(f"\n[{today_date}] User terminated script")
-        break
-    
+        else:
+            print("=" * 25 + "You must choose one of the optiones 1, 2, 3 or 4 !" + 25 * "=")
+            with open("Logs.txt","a") as file:
+                file.write(f"\n[{today_date}] Incorrect menu choice")
+            continue
+        
     else:
-        print("=" * 25 + "You must choose one of the optiones 1, 2 or 3 !" + 25 * "=")
+        print("=" * 25 +"Operative system is not Windows" + 25 * "=")
         with open("Logs.txt","a") as file:
-            file.write(f"\n[{today_date}] Incorrect menu choice")
-        continue
-    
-        
-
-
-
+                file.write(f"\n[{today_date}] Script was run on a {sys.platform} system")
+        break
             
-        
-        
 
-            
+
+
+                    
+                
+                
+
+                    
 
