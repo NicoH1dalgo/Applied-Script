89084295e70a6f7f85b8ca18b27c78d2781765af
NicoH1dalgo <nh@hidalgo.se>
2025-12-18 12:05:43 +0100

    Added correct script in ö1

--- ö1_Info_Script/script.sh
+++ ö1_Info_Script/script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #
 # Detta script samlar in systeminformation - RECON
 #
@@ -8,27 +9,53 @@
 # Author: Frans Schartau
 # Last Update: 2025-01-01
 
-
-echo "Välkommen till RECON script för att kontrollera en Linux-miljö"
+echo "Välkommen till mitt RECON script för att kontrollera en Linux-miljö"
 
 echo
 echo "=== SYSTEMINFO ==="
 uname -a
+date
+df
+ps
+echo
+uptime
+
+echo
+env | grep auth.sock
+echo
+du
 
 echo
 echo "=== AKTUELL ANVÄNDARE ==="
 echo $USER
+w
+id
+groups
 
 echo
 echo "=== ANVÄNDARE MED SHELL ==="
-grep "sh$" /etc/passwd
+cat /etc/passwd | grep "sh$"
+
 
 echo
 echo "=== NÄTVERK ==="
 ip a | grep inet
+ifconfig -a | grep -A 1 wlan
+echo
+echo "=== HÄMTA PUBLIK IP INFO  ==="
+curl ipinfo.io/ip
+arp
 
 echo
-echo "=== Cheat sheet  ==="
-curl cheat.sh
+echo "=== RAM ANVÄNDNING  ==="
+free -h
 
 
+echo
+echo "=== CPU INFO  ==="
+lscpu | grep Core
+
+
+#
+# skriv in dina kommandon för tester
+#
